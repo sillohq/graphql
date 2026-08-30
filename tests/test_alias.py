fd6af78e966867462e871240ff30b06f043726d7
Chidebele Dunamis <techwithdunamix@gmail.com>
2026-08-30 13:31:00 +0100

    tests(alias): cover TestResolve

--- tests/test_alias.py
+++ tests/test_alias.py
@@ -13,3 +13,24 @@ import sys
 import pytest
 
 import _sillo_graphql_bootstrap as bootstrap
+
+
+class TestResolve:
+    def test_the_alias_maps_to_the_real_package(self):
+        assert bootstrap._resolve("sillo.graphql") == "sillo_graphql"
+
+    def test_a_submodule_maps_through(self):
+        assert bootstrap._resolve("sillo.graphql.limits") == "sillo_graphql.limits"
+
+    def test_a_deep_submodule_maps_through(self):
+        assert (
+            bootstrap._resolve("sillo.graphql.transport.ws")
+            == "sillo_graphql.transport.ws"
+        )
+
+    @pytest.mark.parametrize(
+        "name",
+        ["sillo", "sillo.record", "sillo_graphql", "graphql", "sillo.graphqlish"],
+    )
+    def test_anything_else_is_declined(self, name):
+        assert bootstrap._resolve(name) is None
